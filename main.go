f5af48ced4ab9f73e8bbde8ebba744f53f7fdd8e
RiKaCC <chenruijia_cc@163.com>
2017-12-08 11:48:51 +0800

    Update main.go
    add X-Amz-Date header

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
-
+	"time"
 	"github.com/bmizerany/aws4"
 	"github.com/davecgh/go-spew/spew"
 )
@@ -12,6 +12,10 @@ import (
 func main() {
 	r, _ := http.NewRequest("GET", "http://kls.cn-beijing-6.api.ksyun.com?Action=ListStreamDurations&Version=2017-01-01&UniqueName=maxi&App=live&Pubdomain=live.moxiulive.com&StartUnixTime=1492617600&EndUnixTime=1492704000", nil)
 
+	now := time.Now().UTC().Format("2006-01-02 15:04:05")
+	amzDate := now[0:4] + now[5:7] + now[8:10] + "T" + now[11:13] + now[14:16] + now[17:19] + "Z"
+	r.Header.Set("X-Amz-Date", amzDate)
+	
 	s := &aws4.Service{
 		Name:   "kls",
 		Region: "cn-beijing-6",
